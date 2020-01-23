f6ddc70c7a994418c80f760be4e86ad0a4db99b9
fadeomar <fadeomar2015@gmail.com>
2020-01-23 15:47:13 +0200

    add valuation section and style it  and edit the header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import HeaderSection from "./sections/header";
+import Valuation from "./sections/valuation";
 
 function App() {
   return (
     <div className="App">
       <HeaderSection />
+      <Valuation />
     </div>
   );
 }

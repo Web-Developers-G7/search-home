90f4e9108d8e427499de1bb279c44893b0daf7d6
AlaaTaima <alaa.ktaima@gmail.com>
2020-01-26 14:42:42 +0200

    remove unused lines Relates #6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import HeaderSection from './sections/header';
 import TopRated from './sections/topRated';
 import PopularSection from './sections/popular';

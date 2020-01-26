be384c4ad2c00190ec2016700ef3f109edba0755
fadeomar <fadeomar2015@gmail.com>
2020-01-26 23:22:03 +0200

    export the CityProperty component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import HeaderSection from './sections/header';
-import TopRated from './sections/topRated';
-import PopularSection from './sections/popular';
-import Valuation from './sections/valuation';
-import RecentSection from './sections/recent';
-import Footer from './sections/footer';
-import './App.css';
+import React from "react";
+import "antd/dist/antd.css";
+import HeaderSection from "./sections/header";
+import TopRated from "./sections/topRated";
+import PopularSection from "./sections/popular";
+import Valuation from "./sections/valuation";
+import RecentSection from "./sections/recent";
+import Footer from "./sections/footer";
+import CityProperty from "./sections/CityProperty";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <HeaderSection />
+      <CityProperty />
       <TopRated />
       <PopularSection />
       <Valuation />

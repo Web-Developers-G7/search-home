16778607c62cdf1b7b32b29aff260597ad76d2b7
fadeomar <fadeomar2015@gmail.com>
2020-01-26 23:20:32 +0200

    create and export the City Comp.

---
+++ src/sections/CityProperty/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Carousel } from "antd";
+import { data } from "./data";
+import Card from "./card";
+import { Title, SubTitle, SubSection } from "../../components/popular";
+
+export class CityProperty extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      arrows: true,
+      infinite: false,
+      fade: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+    return (
+      <>
+        <SubSection>
+          <Title>Browse popular properties around the world</Title>
+          <SubTitle>Find Properties In Your City</SubTitle>
+        </SubSection>
+        <Carousel {...settings}>
+          {data.map(card => (
+            <Card card={card} key={card.url} />
+          ))}
+        </Carousel>
+      </>
+    );
+  }
+}
+
+export default CityProperty;

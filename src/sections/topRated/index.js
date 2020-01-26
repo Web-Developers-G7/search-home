d78e0856993d1da2c40ce3615c082e98cdb90b80
fadeomar <fadeomar2015@gmail.com>
2020-01-26 16:17:24 +0200

    add title to the toprated section

--- src/sections/topRated/index.js
+++ src/sections/topRated/index.js
@@ -17,6 +17,7 @@ import {
   PriceSpan,
   LocationP
 } from "../../components/topRated";
+import { Title, SubTitle, SubSection } from "../../components/popular";
 import "./style.css";
 
 class TopRated extends React.Component {
@@ -24,6 +25,7 @@ class TopRated extends React.Component {
   componentDidMount() {
     this.setState({ data: Data });
   }
+
   carousel = React.createRef();
 
   next = () => {
@@ -72,67 +74,73 @@ class TopRated extends React.Component {
     };
 
     return (
-      <Carousel {...settings} ref={node => (this.carousel = node)}>
-        {data
-          ? data.map(card => (
-              <div className="rated-section">
-                <img className="rated-image" src={card.url} alt="pic" />
-                <Icon
-                  type="right"
-                  className="right-icons icons"
-                  onClick={this.next}
-                />
-                <Icon
-                  type="left"
-                  className="left-icons icons"
-                  onClick={this.previous}
-                />
-                <div className="rated-detail">
-                  <DetailCard>
-                    <Place>
-                      <Name className="title">{card.name}</Name>
-                      <Location className="title_icon">
-                        <Icon type="environment" theme="filled" color="red" />
-                        <LocationP> {card.location}</LocationP>
-                      </Location>
-                    </Place>
-                    <Details className="detailIcon">
-                      <TopDetail>
-                        <DetailSpan>
-                          <Icon type="bank" theme="filled" />
-                          <DetailSpan>{card.detail1}</DetailSpan>
-                        </DetailSpan>
-                        <DetailSpan>
-                          <Icon type="compass" theme="filled" />
-                          <DetailSpan>{card.detail2}</DetailSpan>
-                        </DetailSpan>
-                      </TopDetail>
-                      <BottomDetail>
-                        <DetailSpan>
-                          <Icon type="switcher" theme="filled" />
-                          <DetailSpan>{card.detail3}</DetailSpan>
-                        </DetailSpan>
-                        <DetailSpan>
-                          <Icon type="car" theme="filled" />
-                          <DetailSpan>{card.detail4}</DetailSpan>
-                        </DetailSpan>
-                      </BottomDetail>
-                    </Details>
-                    <Price>
-                      <Rate className="rateIcon">
-                        <Icon type="star" theme="filled" />
-                        <Icon type="star" theme="filled" />
-                        <Icon type="star" theme="filled" />
-                        <Icon type="star" theme="filled" />
-                      </Rate>
-                      <PriceSpan>Starts From / {card.price} </PriceSpan>
-                    </Price>
-                  </DetailCard>
+      <>
+        <SubSection>
+          <Title>Check out some of our</Title>
+          <SubTitle>Featured Property</SubTitle>
+        </SubSection>
+        <Carousel {...settings} ref={node => (this.carousel = node)}>
+          {data
+            ? data.map(card => (
+                <div className="rated-section">
+                  <img className="rated-image" src={card.url} alt="pic" />
+                  <Icon
+                    type="right"
+                    className="right-icons icons"
+                    onClick={this.next}
+                  />
+                  <Icon
+                    type="left"
+                    className="left-icons icons"
+                    onClick={this.previous}
+                  />
+                  <div className="rated-detail">
+                    <DetailCard>
+                      <Place>
+                        <Name className="title">{card.name}</Name>
+                        <Location className="title_icon">
+                          <Icon type="environment" theme="filled" color="red" />
+                          <LocationP> {card.location}</LocationP>
+                        </Location>
+                      </Place>
+                      <Details className="detailIcon">
+                        <TopDetail>
+                          <DetailSpan>
+                            <Icon type="bank" theme="filled" />
+                            <DetailSpan>{card.detail1}</DetailSpan>
+                          </DetailSpan>
+                          <DetailSpan>
+                            <Icon type="compass" theme="filled" />
+                            <DetailSpan>{card.detail2}</DetailSpan>
+                          </DetailSpan>
+                        </TopDetail>
+                        <BottomDetail>
+                          <DetailSpan>
+                            <Icon type="switcher" theme="filled" />
+                            <DetailSpan>{card.detail3}</DetailSpan>
+                          </DetailSpan>
+                          <DetailSpan>
+                            <Icon type="car" theme="filled" />
+                            <DetailSpan>{card.detail4}</DetailSpan>
+                          </DetailSpan>
+                        </BottomDetail>
+                      </Details>
+                      <Price>
+                        <Rate className="rateIcon">
+                          <Icon type="star" theme="filled" />
+                          <Icon type="star" theme="filled" />
+                          <Icon type="star" theme="filled" />
+                          <Icon type="star" theme="filled" />
+                        </Rate>
+                        <PriceSpan>Starts From / {card.price} </PriceSpan>
+                      </Price>
+                    </DetailCard>
+                  </div>
                 </div>
-              </div>
-            ))
-          : null}
-      </Carousel>
+              ))
+            : null}
+        </Carousel>
+      </>
     );
   }
 }
